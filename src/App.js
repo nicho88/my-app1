ff802e7fa4324a53a58ea4defe374a3fd676da5b
Nicho Ardiansyah <nichoardiansyah@Nichos-MBP.lan>
2023-06-13 21:29:50 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./pages/Header"
+// import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+// import React from 'react'
+// import Travel from '../assets/images/travel.jpg'
+
+// function Home() {
+//   return (
+//     <div>
+//       <div className='flex justify-end h-80 bg-cover bg-center' style={{backgroundImage: `url(${Travel})` }}>
+//         <div className='m-5'>
+//             <ul className='flex'>
+//                 <li className='px-5 py-1 bg-white hover:bg-gray-400 cursor-pointer text-[#566FA5] mr-5'>Login</li>
+//                 <li className='px-5 py-1 bg-[#566FA5] hover:bg-[#687FAF] cursor-pointer text-white'>Register</li>
+//             </ul>
+//         </div>
+//       </div>
+
+//         <div className='container mx-auto mt-5'>
+//             <div className='flex'>
+//                 <input type="text" className='p-2 bg-gray-100 w-full mr-3' />
+//                 <button className='bg-[#566FA5] px-10 py-3 text-white cursor-pointer hover:bg-[#687FAF]'>Search</button>
+//             </div>
+//         </div>
+
+//         <div>
+//             <div className=''>
+//                 <ul className='flex mt-5 justify-center'>
+//                     <li className='py-1 px-5'>Add Member</li>
+//                     <button className='bg-[#008000] text-white cursor-pointer hover:bg-[#008080] px-5 py-1'>Add</button>
+//                 </ul>
+//             </div>
+//         </div>
+
+        
+
+//     </div>
+//   )
+// }
+
+// export default Home
