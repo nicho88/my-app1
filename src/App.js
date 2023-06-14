90d021d87097b858446ae82a863fa3ee16bb4d5c
Nicho Ardiansyah <nichoardiansyah@Nichos-MBP.lan>
2023-06-14 22:59:03 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
+import { useState } from "react"
 import Header from "./pages/Header"
+import Tasks from "./pages/Tasks"
+import AddTask from "./pages/AddTask"
+
 // import Home from "./pages/Home"
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState ([
+    {
+      id: 1,
+      text: 'Token1 : 0x790DffD2371dDDC693',
+      day: 'jun 14th at 01:55pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Token2 : 0x790DffD2371dDDC693',
+      day: 'jul 15th at 02:55pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Token3 : 0x790DffD2371dDDC693',
+      day: 'Aug 16th at 03:55pm',
+      reminder: false,
+    },
+])
+
+// add task
+const addTask = (task) => {
+  console.log(task)
+}
+
+// delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !==id))
+}
+
+// toggle reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) =>
+    task.id === id  ? { ...task, reminder: 
+    !task.reminder } : task
+    )
+  )
+}
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ?(
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder}/>
+      ): (
+        'No Member'
+      )}
     </div>
   )
 }
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,25 +1,33 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('click')
+    }
+    
+
     return (
-        <header>
-            <h1 style={headingStyle}>{title}</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
+            
         </header>
     )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
+    title: 'Member',
 }
 
 Header.prototype = {
     title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-    color: 'red',
-    backgroundColor: 'black',
-}
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
 
 
 
